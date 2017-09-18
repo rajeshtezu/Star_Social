c706921c73081d2e54b809b3d7b7afaf9ef1ce89
Rajesh Kumar <rajeshtezu90@gmail.com>
2017-09-18 21:43:50 +0530

    added bootstrap3

--- starSocial/settings.py
+++ starSocial/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'ezvxhvceqout#$c7wfpbfqsqds3lpp6y-ib90w1qk8my#&45!+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['c029hkwn.apps.lair.io']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrap3',
     'accounts',
 ]
 
