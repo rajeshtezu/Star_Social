55a625f952e0af18e42f538abce295bb2a0d01d8
Rajesh Kumar <rajeshtezu90@gmail.com>
2017-09-19 08:54:25 +0530

    added Group and GroupMember models

--- groups/models.py
+++ groups/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.utils.text import slugify # remove chars which are not alphanumeric
+import misaka  # allow markdown
 
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+from django import template
+register = template.Library()
 # Create your models here.
+
+class Group(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    slug = models.SlugField(allow_unicode=True, unique=True)
+    description = models.TextField(blank=True, default='')
+    description_html = models.TextField(editable=False, default='', blank=True)
+    members = models.ManyToManyField(User, through='GroupMember')
+
+    def __str__(self):
+        return self.name
+
+    def save(self):
+        self.slug = slugify(self.name)
+        self.description_html = misaka.html(self.description)
+        super().save(*args, **kwargs)
+
+    def get_absolue_url(self):
+        return reverse('group:single', kwargs={'slug':self.slug})
+
+class GroupMember(models.Model):
+    group = models.ForeignKey(Group, related_name='memberships')
+    user = models.ForeignKey(User, related_name='user_groups')
+
+    def __str__(self):
+        return self.user.username
+
+    class Meta:
+        unique_together = ('group', 'user')
