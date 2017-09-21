31ad9d7327f5b0ca4e1fd9c3e9bde7cc559fc8d4
Rajesh Kumar <rajeshtezu90@gmail.com>
2017-09-21 07:39:35 +0530

    view to join group

--- groups/views.py
+++ groups/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
 from django.contrib.auth.mixins import (LoginRequiredMixin, PermissionRequiredMixin)
 from django.core.urlresolvers import reverse
 from django.views import generic
@@ -11,18 +12,26 @@ class CreateGroup(LoginRequiredMixin, generic.CreateView):
     fields = ('name', 'description')
     model = Group
 
-    
+
 class SingleGroup(generic.DetailView):
     model = Group
-    
+
 class ListGroup(generic.ListView):
     model = Group
-    
-    
-    
-    
-    
-    
-    
-    
-    
+
+class Joingroup(LoginRequiredMixin, generic.RedirectView):
+
+    def get_redirect_url(self, *args, **kwargs):
+        return reverse('groups:single', kwargs={'slug':self.kwargs.get('slug')})
+
+    def get(self, request, *args, **kwargs):
+        group = get_object_or_404(Group, slug=self.kwargs.get('slug'))
+
+        try:
+            GroupMember.objects.create(user=self.request.user, group=group)
+        except IntegrityError:
+            messages.warning(self.request, 'Warning : Already a member!')
+        else:
+            messages.success(self.request, 'You are now a member!')
+
+        return super().get(request, *args, **kwargs)
