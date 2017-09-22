dae8f11879e2fe5a655aebf8924b522e29a02a75
Rajesh Kumar <rajeshtezu90@gmail.com>
2017-09-22 08:39:14 +0530

    corrected STATICFILES_DIRS

--- starSocial/settings.py
+++ starSocial/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'ezvxhvceqout#$c7wfpbfqsqds3lpp6y-ib90w1qk8my#&45!+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['c029hkwn.apps.lair.io']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -122,7 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIR = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 LOGIN_REDIRECT_URL = 'test'
 LOGOUT_REDIRECT_URL = 'thanks'
