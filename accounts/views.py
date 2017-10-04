a7330f85efea5f0a035f794b75cce24907c4e5ef
Rajesh Kumar <rajeshtezu90@gmail.com>
2017-10-04 09:30:18 +0530

    removed unnecessary url

--- accounts/views.py
+++ accounts/views.py
@@ -7,6 +7,5 @@ from . import forms
 # Create your views here.
 class SignUp(CreateView):
     form_class = forms.UserCreateForm
-    success_url = reverse_lazy('login')
+    success_url = reverse_lazy('accounts:login')
     template_name = 'accounts/signup.html'
-    
--- starSocial/urls.py
+++ starSocial/urls.py
@@ -21,7 +21,7 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.HomePage.as_view(), name="home"),
     url(r'^accounts/', include('accounts.urls', namespace='accounts')),
-    url(r'^accounts/', include('django.contrib.auth.urls')),
+    #url(r'^accounts/', include('django.contrib.auth.urls')), # used if we need extra built-in functionality [https://github.com/django/django/blob/master/django/contrib/auth/urls.py]
     url(r'^test/$', views.TestPage.as_view(), name='test'),
     url(r'^thanks/$', views.ThanksPage.as_view(), name='thanks'),
     url(r'^posts/', include('posts.urls', namespace='posts')),
